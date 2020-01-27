1e6939a0094f1a99045b6434f9b5729f8ecc3044
Christian Wiegand <cwiegand@wgdnet.de>
2020-01-27 19:52:59 +0100

    Update requirements

--- photos/settings.py
+++ photos/settings.py
@@ -71,7 +71,7 @@ WSGI_APPLICATION = 'photos.wsgi.application'
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'de-de'
+LANGUAGE_CODE = 'de'
 TIME_ZONE = 'Europe/Berlin'
 USE_I18N = True
 USE_L10N = True
