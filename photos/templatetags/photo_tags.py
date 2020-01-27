7111d0f410eaf2ce2ff898ac666dd1407b172afd
Christian Wiegand <cwiegand@wgdnet.de>
2020-01-27 22:22:47 +0100

    Show only tags and events with visible photos

--- photos/templatetags/photo_tags.py
+++ photos/templatetags/photo_tags.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 from django import template
-
+from django.db.models import Q
 
 register = template.Library()
 
@@ -15,6 +15,17 @@ def cut_photos(photos, n):
     return photos
 
 
+@register.simple_tag
+def get_visibles(photos, user):
+    """
+    get only photos visible for user
+    """
+    return photos.filter(
+        Q(owner=user) |
+        Q(shared=user)
+    )
+
 @register.filter(name='rotation')
 def rotation(value):
     return value * 10 - 20
+
