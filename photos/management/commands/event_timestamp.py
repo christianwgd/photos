018a13bbac011674eb64d2902d2da2be5e18a720
Christian Wiegand <cwiegand@wgdnet.de>
2020-05-14 09:57:54 +0200

    Add timestamp to event

--- photos/management/commands/event_timestamp.py
+++ photos/management/commands/event_timestamp.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from photos.models import Event
+from photos.models import Event, Photo
 
 
 class Command(BaseCommand):
@@ -12,10 +12,17 @@ class Command(BaseCommand):
         for event in events:
             if event.timestamp is None:
                 photos = event.photo_set.all()
-                latest = photos.latest('timestamp')
-                event.timestamp = latest.timestamp
-                event.save()
-                count += 1
+                try:
+                    latest = photos.latest('timestamp')
+                    event.timestamp = latest.timestamp
+                    event.save()
+                    count += 1
+                except Photo.DoesNotExist:
+                    self.stdout.write(self.style.WARNING(
+                        'Event {event} has no photos with timestamp'.format(
+                            event=event.name
+                        )
+                    ))
 
         self.stdout.write(self.style.SUCCESS(
             'Timestamp of {count} events successfully synchronized'.format(
