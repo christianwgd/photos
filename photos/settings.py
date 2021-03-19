236c63b7cc1ca9947e620625a470725310a1f60c
Christian Wiegand <cwiegand@wgdnet.de>
2021-03-19 11:25:31 +0100

    Update requirements

--- photos/settings.py
+++ photos/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'drf_spectacular',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
