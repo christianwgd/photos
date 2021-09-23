4954a946544604fb2788c7c54c7f80c7cd2a267b
Christian Wiegand <christianwgd@users.noreply.github.com>
2021-09-23 15:56:24 +0200

    Update requirements.txt, set dynamic theme-color meta tag

--- usersettings/templatetags/user_tags.py
+++ usersettings/templatetags/user_tags.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+import re
 from datetime import timedelta, date
 
 from django import template
@@ -30,6 +31,23 @@ def user_theme(user):
         url=theme_url
     )
     return mark_safe(theme_tag)
+
+
+@register.simple_tag
+def user_theme_primary_color(user):
+    theme_url = '/photos/themes/bootstrap.min.css'
+    if user.is_authenticated:
+        try:
+            user_settings = UserSettings.objects.get(user=user)
+            if user_settings.theme is not None:
+                theme_url = user_settings.theme.cssfile.url
+        except UserSettings.DoesNotExist:
+            pass
+    media_url = getattr(settings, 'MEDIA_ROOT', None)
+    theme_url = media_url.replace('/media', '') + theme_url
+    with open(theme_url) as theme:
+        color = re.findall('--primary:\s*#[a-fA-Ff0-9]{6}', theme.read())[0].split(':')[1]
+    return color
 
 
 @register.simple_tag
